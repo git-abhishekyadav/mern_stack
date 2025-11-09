b5b70bbd15720d46b39f24a0b7d1f3123c10f051
git-abhishekyadav <abhishekyadav28@outlook.com>
2025-11-09 22:50:53 +0530

    fix: main.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
+const App = () => (
+    <h1>Hello World</h1>
+)
 
-import './App.css';
-import WelcomeH5 from './WelcomeH4';
-import {Button } from './Button';
-import { Hello, HelloWithoutJSX } from './Hello';
-import { Card, CardWithoutJSX } from './Card';
-import { Rule4 } from './Rules';
-
-function Welcome() {
-  return (
-    <h3>Welcome to React</h3>
-  )
-}
-
-function App() {
-
-  return (
-    <div>
-      <Hello />
-      <HelloWithoutJSX />
-      <Welcome />
-      <WelcomeH5 />
-      <Button />
-      <Card />
-      <CardWithoutJSX />
-      <Rule4 />
-    </div>
-  )
-}
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import React from "react";
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const App = () => (
+  <div>Hello World</div>,
+    <div>Hello Abhi</div>
+
 )
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// root.render(App);  cant render function it needs to render component as above line
+ 
